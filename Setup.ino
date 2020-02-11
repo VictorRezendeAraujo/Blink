f6549c90413df6182f733cafe2db20340ae4a741
JulioIten <60941080+JulioIten@users.noreply.github.com>
2020-02-11 16:24:59 -0300

    loop
    codigo para acender o LED, esperar 1 segundo e apagar o LED

--- Setup.ino
+++ Setup.ino
@@ -3,4 +3,8 @@ void setup(){
  pinMode(LED_BUILTIN, OUTPUT);//Define a porta do LED como "OUTPUT".
 }
 void loop(){
+  digitalWrite(LED_BUILTIN, HIGH);   // Acende o LED
+  delay(1000);                       // Espera 1000ms(1 segundo)
+  digitalWrite(LED_BUILTIN, LOW);    // Desliga o LED
+  delay(1000);                       // Espera 1000ms(1 segundo)
 }
